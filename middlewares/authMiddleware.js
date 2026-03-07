fd970a895758e9e3575d046536db84213615bc20
thecircle-team-hub <thecircleofficialx@gmail.com>
2026-03-07 06:23:44 -0300

    Implement JWT authentication middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,26 @@
+const jwt = require("jsonwebtoken");
 
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token inválido" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Token inválido" });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authMiddleware;
